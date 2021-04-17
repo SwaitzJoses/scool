ed7e16dfa7bbfe921202f6c3a7a61bc429ca3c91
Swaitz Joses <swaitz.joses@gmail.com>
2021-04-17 19:33:19 +0530

    hihi

--- src/Components/Course.js
+++ src/Components/Course.js
@@ -14,11 +14,24 @@ function Course() {
             <li>
               Digital Marketing need get started 101 is the ultimate primer{" "}
             </li>
+            <li>
+              Digital Marketing need get started 101 is the ultimate primer{" "}
+            </li>
+            <li>
+              Digital Marketing need get started 101 is the ultimate primer{" "}
+            </li>
           </ul>
         </div>
       </Col>
       <Col sm={8}>
-        <div className="side2">;</div>
+        <div className="side2">
+        <div className="s2-title">Course Overview </div>
+        <hr className="s2-line"></hr>
+        <div className = "s2-des">Digital Marketing 101 is the ultimate primer you need to get started with a career in Digital marketing. This course is a Digital Marketing 101 is the ultimate primer you need to get started with a career in Digital marketing. This course isDigital Marketing 101 is the ultimate primer you need to get started.<br /><br />
+         1. Digital Marketing 101 is the ultimate primer<br />
+         2. Digital Marketing 101 is the ultimate primer <br />
+         3. Digital Marketing 101 is the ultimate primer</div>
+        </div>
       </Col>
     </Row>
   );
