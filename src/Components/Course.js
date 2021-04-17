854992ae36696b2899c6e114e49b80bceb1e7834
Swaitz Joses <swaitz.joses@gmail.com>
2021-04-17 22:12:49 +0530

    hi

--- src/Components/Course.js
+++ src/Components/Course.js
@@ -4,36 +4,142 @@ import { Container, Row, Col } from "react-bootstrap";
 
 function Course() {
   return (
-    <Row>
-      <Col sm={4}>
-        <div className="side1">
-          <div className="s1-title">Key Takeaways </div>
-          <hr className="s1-line"></hr>
+    <>
+      <Row>
+        <Col sm={4}>
+          <div className="side1">
+            <div className="s1-title">Key Takeaways </div>
+            <hr className="s1-line"></hr>
 
-          <ul>
-            <li>
-              Digital Marketing need get started 101 is the ultimate primer{" "}
-            </li>
-            <li>
-              Digital Marketing need get started 101 is the ultimate primer{" "}
-            </li>
-            <li>
-              Digital Marketing need get started 101 is the ultimate primer{" "}
-            </li>
-          </ul>
-        </div>
-      </Col>
-      <Col sm={8}>
-        <div className="side2">
-        <div className="s2-title">Course Overview </div>
-        <hr className="s2-line"></hr>
-        <div className = "s2-des">Digital Marketing 101 is the ultimate primer you need to get started with a career in Digital marketing. This course is a Digital Marketing 101 is the ultimate primer you need to get started with a career in Digital marketing. This course isDigital Marketing 101 is the ultimate primer you need to get started.<br /><br />
-         1. Digital Marketing 101 is the ultimate primer<br />
-         2. Digital Marketing 101 is the ultimate primer <br />
-         3. Digital Marketing 101 is the ultimate primer</div>
-        </div>
-      </Col>
-    </Row>
+            <ul>
+              <li>
+                Digital Marketing need get started 101 is the ultimate primer{" "}
+              </li>
+              <li>
+                Digital Marketing need get started 101 is the ultimate primer{" "}
+              </li>
+              <li>
+                Digital Marketing need get started 101 is the ultimate primer{" "}
+              </li>
+            </ul>
+          </div>
+        </Col>
+        <Col sm={8}>
+          <div className="side2">
+            <div className="s2-title">Course Overview </div>
+            <hr className="s2-line"></hr>
+            <div className="s2-des">
+              Digital Marketing 101 is the ultimate primer you need to get
+              started with a career in Digital marketing. This course is a
+              Digital Marketing 101 is the ultimate primer you need to get
+              started with a career in Digital marketing. This course isDigital
+              Marketing 101 is the ultimate primer you need to get started.
+              <br />
+              <br />
+              1. Digital Marketing 101 is the ultimate primer
+              <br />
+              2. Digital Marketing 101 is the ultimate primer <br />
+              3. Digital Marketing 101 is the ultimate primer
+            </div>
+          </div>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col sm={8}>
+          <div className="side4">
+            <iframe
+              className="video"
+              src="https://www.youtube.com/embed/tgbNymZ7vqY"
+            ></iframe>
+          </div>
+        </Col>
+        <Col sm={4}>
+          <div className="side5">
+            <div className="s1-title">Is this EMC for you?</div>
+            <hr className="s1-line"></hr>
+            <div className="s5-des">
+              Digital Marketing 101 is the ultimate primer you need to get
+              started with a career in Digital marketing. This course is a
+              Digital Marketing 101 is the ultimate primer you need to get
+              started with a career in Digital marketing. This course isDigital
+              Marketing 101 is the ultimate primer you need to get
+              started.Digital Marketing 101 is the ultimate primer you need to
+              get started with a career in Digital marketing. This course is a
+              Digital Marketing 101 is the ultimate primer you need to get
+              started with a career in Digital marketing.
+            </div>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col sm={4}>
+          <div className="side6">
+            <div className="s1-title">About this Mentor </div>
+            <hr className="s1-line"></hr>
+
+            <div className="s5-des">
+              Digital Marketing 101 is the ultimate primer you need to get
+              started with a career in Digital marketing. This course is a
+              Digital Marketing 101 is the ultimate primer you need to get
+              started with a career in Digital marketing. This course isDigital
+              Marketing 101 is the ultimate primer you need to get
+              started.Digital Marketing 101 is the ultimate primer you need to
+              get started with a career in Digital marketing. This course is a
+              Digital Marketing 101 is the ultimate primer you need to get
+              started with a career in Digital marketing. This course isDigital
+              Marketing 101 is the ultimate primer you need to get
+              started.Digital Marketing 101 is the with a career in Digital
+              marketing. This course is a Digital Marketing 101 is the ultimate
+              primer you need to get started with a career in Digital marketing.
+              This course isDigital Marketing 101 is the ultimate primer you
+              need to get started.Digital Marketing 101 is the Digital Marketing
+              101 is the Digital Marketing.
+            </div>
+          </div>
+        </Col>
+        <Col sm={8}>
+          <div className="side7">
+            <div className="s2-title">Why ‘Experiential Micro-Course’? </div>
+            <hr className="s2-line"></hr>
+            <div className="s2-des">
+              Digital Marketing 101 is the ultimate primer you need to get
+              started with a career in Digital marketing. This course is a
+              Digital Marketing 101 is the ultimate primer you need to get
+              started with a career in Digital marketing. This course isDigital
+              Marketing 101 is the ultimate primer you need to get started.
+              <br />
+              <br />
+              1. Digital Marketing 101 is the ultimate primer
+              <br />
+              2. Digital Marketing 101 is the ultimate primer <br />
+              3. Digital Marketing 101 is the ultimate primer
+              <br />
+              <br />
+              Digital Marketing 101 is the ultimate primer you need to get
+              started with a career in Digital marketing. This course is a
+              Digital Marketing 101 is the ultimate primer you need to get
+              started with a career in Digital marketing. This course isDigital
+              Marketing 101 is the ultimate primer you need to get started.
+              <br />
+              <br />
+              1. Digital Marketing 101 is the ultimate primer
+              <br />
+              2. Digital Marketing 101 is the ultimate primer <br />
+              3. Digital Marketing 101 is the ultimate primer
+              <br />
+              <br />
+              Digital Marketing 101 is the ultimate primer you need to get
+              started with a career in Digital marketing. This course is a
+              Digital Marketing 101 is the ultimate primer you need to get
+              started with a career in Digital marketing. This course isDigital
+              Marketing 101 is the ultimate primer you need to get started.
+              
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </>
   );
 }
 
